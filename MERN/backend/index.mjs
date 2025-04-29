6545a4e0d653c010381edfe7f453e6992303a63f
Amna-Attaria <amna12khan26@gmail.com>
2025-04-28 23:17:09 -0700

    complete

--- MERN/backend/index.mjs
+++ MERN/backend/index.mjs
@@ -12,7 +12,6 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'https://mern-hackathone-dwba.vercel.app',
-  'https://mern-hackathone-1mkr.vercel.app'
 ];
 
 const corsOptions = {
